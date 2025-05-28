c65ff50655611ae6c6cda656942ac79de42de23a
your_name <orkunkorcak@gmail.com>
2025-05-28 17:33:39 +0300

    edited2 server js

--- src/server.js
+++ src/server.js
@@ -66,12 +66,12 @@ export const startServer = () => {
     });
   });
 
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
+  // app.use((err, req, res, next) => {
+  //   res.status(500).json({
+  //     message: 'Something went wrong',
+  //     error: err.message,
+  //   });
+  // });
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
