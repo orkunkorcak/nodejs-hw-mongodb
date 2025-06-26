843415f8ae56ef0c70694c1a469ff7f2480c8c44
your_name <orkunkorcak@gmail.com>
2025-06-26 17:15:43 +0300

    token services added

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { registerUserSchema, loginUserSchema } from '../validation/auth.js';
 import { registerUserController, loginUserController } from '../controllers/auth.js';
-import { validateBody } from '../middlewares/validateBody.js';
+import  validateBody  from '../middlewares/validateBody.js';
 
 const router = Router();
 
