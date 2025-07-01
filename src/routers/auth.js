2293aad80aa1e368ab688e7e98ceb53c44dc8ae9
your_name <orkunkorcak@gmail.com>
2025-07-02 00:59:38 +0300

    email service added

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -6,6 +6,10 @@ import { registerUserController, loginUserController } from '../controllers/auth
 import validateBody from '../middlewares/validateBody.js';
 import { logoutUserController } from '../controllers/auth.js';
 import { refreshUserSessionController } from '../controllers/auth.js';
+import { sendResetEmailSchema } from '../validation/auth.js';
+import { sendResetEmailController } from '../controllers/auth.js';
+import { resetPasswordSchema } from '../validation/auth.js';
+import { resetPasswordController } from '../controllers/auth.js';
 
 
 const router = Router();
@@ -25,5 +29,14 @@ router.post(
   ctrlWrapper(logoutUserController),
 );
 router.post('/refresh', ctrlWrapper(refreshUserSessionController));
-
+router.post(
+  '/send-reset-email',
+  validateBody(sendResetEmailSchema),
+  ctrlWrapper(sendResetEmailController),
+);
+router.post(
+  '/reset-password',
+  validateBody(resetPasswordSchema),
+  ctrlWrapper(resetPasswordController),
+);
 export default router;
