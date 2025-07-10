40f63631e6372befc56a6d5641d9b393b9c62d29
your_name <orkunkorcak@gmail.com>
2025-07-10 15:06:24 +0300

    documentation added for contacts

--- src/constants/index.js
+++ src/constants/index.js
@@ -23,3 +23,4 @@ export const CLOUDINARY = {
   API_KEY: 'API_KEY',
   API_SECRET: 'API_SECRET',
 };
+export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
--- src/server.js
+++ src/server.js
@@ -7,6 +7,8 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
+
 
 
 
@@ -41,6 +43,8 @@ export const startServer = () => {
   app.use(router);
   
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
+
   
   app.use(notFoundHandler);
 
